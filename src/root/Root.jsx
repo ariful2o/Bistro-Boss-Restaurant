e47f3f8ebfb5bd7289ec140793f8fceac42080cb
mohammadariful20 <arifulz848@gmail.com>
2024-06-06 18:38:16 +0600

    add footer

--- src/root/Root.jsx
+++ src/root/Root.jsx
@@ -1,12 +1,14 @@
 
 import { Outlet } from 'react-router-dom'
 import Navbar from '../pages/Home/shared/Navbar/Navbar'
+import Footer from '../pages/Home/shared/footer/Footer'
 
 export default function Root() {
   return (
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   )
 }
