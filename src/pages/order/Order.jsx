78487bc6933bc48c9bd3939ebb2212915670dbec
mohammadariful20 <arifulz848@gmail.com>
2024-06-09 14:49:09 +0600

    add menus and banner

--- src/pages/order/Order.jsx
+++ src/pages/order/Order.jsx
@@ -1,6 +1,12 @@
 import Cover from "../../components/cover/Cover";
 import sectionImg from '../../assets/menu/banner3.jpg'
+import dessertImg from '../../assets/menu/dessert-bg.jpeg'
+import pizzaImg from '../../assets/menu/pizza-bg.jpg'
+import saladImg from '../../assets/menu/salad-bg.jpg'
+import soupImg from '../../assets/menu/soup-bg.jpg'
+
 import useMenu from "../../hooks/menus/useMenu";
+import MenuItem from "../../components/menu/MenuItem";
 
 
 export default function Order() {
@@ -8,11 +14,33 @@ export default function Order() {
     const offered = menu.filter(menu => menu.category === "offered")
     const soup = menu.filter(menu => menu.category === "soup")
     const dessert = menu.filter(menu => menu.category === "dessert")
-    const drinks = menu.filter(menu => menu.category === "drinks")
-   console.log(menu)
+    const salad = menu.filter(menu => menu.category === "salad")
     return (
         <section>
             <Cover img={sectionImg} title="OUR MENU" subTitle="Would you like to try a dish?"></Cover>
+            <div className="grid grid-cols-1 md:grid-cols-2  gap-12 my-10 w-11/12 mx-auto">
+                {
+                    offered.map(menu => <MenuItem key={menu._id} menu={menu}></MenuItem>)
+                }
+            </div>
+            <Cover img={soupImg} title="SOUP" subTitle="Would you like to try a dish?"></Cover>
+            <div className="grid grid-cols-1 md:grid-cols-2  gap-12 my-10 w-11/12 mx-auto">
+                {
+                    soup.map(menu => <MenuItem key={menu._id} menu={menu}></MenuItem>)
+                }
+            </div>
+            <Cover img={dessertImg} title="DESSERTS" subTitle="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."></Cover>
+            <div className="grid grid-cols-1 md:grid-cols-2  gap-12 my-10 w-11/12 mx-auto">
+                {
+                    dessert.map(menu => <MenuItem key={menu._id} menu={menu}></MenuItem>)
+                }
+            </div>
+            <Cover img={saladImg} title="salad" subTitle="Would you like to try a dish?"></Cover>
+            <div className="grid grid-cols-1 md:grid-cols-2  gap-12 my-10 w-11/12 mx-auto">
+                {
+                    salad.map(menu => <MenuItem key={menu._id} menu={menu}></MenuItem>)
+                }
+            </div>
         </section>
     )
 }
