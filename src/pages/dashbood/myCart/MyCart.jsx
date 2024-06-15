7b25d7415e4054f94ec4a932c9f2c59f109ec719
mohammadariful20 <arifulz848@gmail.com>
2024-06-16 03:32:42 +0600

    add table

--- src/pages/dashbood/myCart/MyCart.jsx
+++ src/pages/dashbood/myCart/MyCart.jsx
@@ -1,3 +1,4 @@
+import { CiTrash } from "react-icons/ci";
 import SectionTitle from "../../../components/sectionHeading/SectionTitle";
 import useAddtoCart from "../../../hooks/addtoCart/useAddtoCart";
 
@@ -23,8 +24,48 @@ const MyCart = () => {
           </button>
         </div>
       </div>
-      <div className="">
-        
+      <div className="overflow-x-auto">
+        <table className="table mt-10">
+          {/* head */}
+          <thead className="bg-[#D1A054]">
+            <tr>
+              <th></th>
+              <th>Item Image</th>
+              <th>Ittem Name</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {cart.map((item, index) => (
+              <tr key={item._id}>
+                <th>{index + 1}</th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={item.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <h4>{item.name}</h4>
+                </td>
+                <td>{item.price}</td>
+                <th>
+                  <button className="btn btn-error btn-md">
+                    <CiTrash className="text-white text-3xl" />
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
